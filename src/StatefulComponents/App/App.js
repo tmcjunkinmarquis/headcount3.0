32405a1c4f87f51a90e9cc321b4ca045c09a58e9
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-06-25 15:40:10 -0600

    Add lint functionality

--- src/StatefulComponents/App/App.js
+++ src/StatefulComponents/App/App.js
@@ -4,7 +4,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>Welcome To Headcount 2.0
+
+      </div>
     );
   }
 }
--- src/StatefulComponents/App/Test/App.test.js
+++ src/StatefulComponents/App/Test/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+import { shallow, mount, render } from 'enzyme';
+
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
+  // const div = document.createElement('div');
   // ReactDOM.render(<App />, div);
+  const wrapper = shallow(<App/>)
+  expect(wrapper).toBeDefined()
 });

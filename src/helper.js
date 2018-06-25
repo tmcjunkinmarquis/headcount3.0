f628b9a0aab19b53a1d28001d82be834482b8a1d
dsdunn <dsdunnmusic@gmail.com>
2018-06-25 17:30:18 -0600

    Pass iteration 0 tests

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,19 @@
+import kinderData from './data/kindergartners_in_full_day_program.js';
+
 export default class DistrictRepository {
+  constructor(){
+    this.stats = kinderData.reduce((obj, yearObj) => {
+      let key = yearObj.Location;
+      let year = yearObj.TimeFrame;
+      let data = yearObj.Data;
+      if(!obj[key]) {
+        obj[key] = {};
+      }
+      if(!obj[key][year]) {
+        obj[key][year] = data;
+      }
+      return obj;
+    }, {})
+  }
 
 }

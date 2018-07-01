43b0f13e12a218ecf352b446faee52418c0dbb5c
dsdunn <dsdunnmusic@gmail.com>
2018-06-30 20:11:17 -0600

    Complete app test coverage

--- src/StatefulComponents/App/Test/App.test.js
+++ src/StatefulComponents/App/Test/App.test.js
@@ -67,6 +67,49 @@ describe('App',()=>{
     expect(wrapper.state('selectedCards').length).toEqual(0);
   });
 
+  it('selectDistrict should replace second card when selectedCards contains 2 cards', () => {
+
+    const newSelection = 'AGATE 300';
+
+    wrapper.setState({
+      selectedCards: [{
+      location: 'COLORADO',
+      stats: {
+        2004: 0.24,
+        2005: 0.278,
+        2006: 0.337,
+        2007: 0.395,
+        2008: 0.536,
+        2009: 0.598,
+        2010: 0.64,
+        2011: 0.672,
+        2012: 0.695,
+        2013: 0.703,
+        2014: 0.741,
+      }
+    },
+    {
+      location: 'ACADEMY 20',
+      stats: {
+        2004: 0.24,
+        2005: 0.278,
+        2006: 0.337,
+        2007: 0.395,
+        2008: 0.536,
+        2009: 0.598,
+        2010: 0.64,
+        2011: 0.672,
+        2012: 0.695,
+        2013: 0.703,
+        2014: 0.741,
+      }
+    }]
+    });
+
+    wrapper.instance().selectDistrict(newSelection);
+    expect(wrapper.state('selectedCards')[1].location).toEqual(newSelection);
+  })
+
   it('should update the state when search characters are found', () => {
     // setup
     
